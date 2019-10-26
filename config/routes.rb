f4567e00944871bc97fa24a1bfa6f06d56cd584e
suzuyoshi <yoshisu19950418@yahoo.co.jp>
2019-10-26 15:15:03 +0000

    Finish edit-attendances

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#top'
   get '/signup', to: 'users#new'
 
-  # ログイン機能
+
   get    '/login', to: 'sessions#new'
   post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
     member do
       get 'edit_basic_info'
       patch 'update_basic_info'
+      get 'attendances/edit_one_month'
+      patch 'attendances/update_one_month'
     end
     resources :attendances, only: :update
   end
